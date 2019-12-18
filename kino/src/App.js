4831a56d4317ced0179f18764931bb263ea71abb
Romy Ilano <romy.ilano@gmail.com>
2019-12-18 19:21:20 +0000

    added all thet hings

--- kino/src/App.js
+++ kino/src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import whiskybottle from "./../public/blended-whisky-that-boutiquey-whisky-company-whisky.jpg";
+import milroys from "./../public/Milroys.jpg"
 import './App.css';
+
+// here's a component. this stuff is weird
+function Movie() {
+  return <div>Hello!
+    <img src={milroys} className="Milroys-image" />
+  </div>
+  
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <img src={whiskybottle} className="Whisky-image"  alt="Sample Whisky Logo"/>
+          <h2>Whiskey List</h2>
+          <Movie />
+          <img src={milroys} className="Milroys-image" />
         </div>
         <p className="App-intro">
          Dude! get started, edit <code>src/App.js</code> and save to reload.
+         <Movie />
         </p>
       </div>
     );
